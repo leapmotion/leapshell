d740b08ef11f9ede2851cf0a0d707e4fc8b1db36
Victor 'Yes, The Victor' Dods <vdods@leapmotion.com>
2014-04-17 23:06:34 -0700

    Implemented a prioritized-key sorting function for Tile.  This allows one to specify a list of metadata keys on which to sort, where the first key is the first one to sort by, then the second key, etc.

--- include/Tile.h
+++ include/Tile.h
@@ -12,8 +12,13 @@ public:
   Vector3 m_position;
   Vector3 m_size;
   std::shared_ptr<HierarchyNode> m_node;
+
+  // TODO: should this be eigen-aligned?
 };
 
 typedef std::vector<Tile, Eigen::aligned_allocator<Tile>> TileVector;
+typedef std::vector<std::shared_ptr<Tile>> TilePointerVector;
+
+void SortTiles (TilePointerVector &v, std::vector<std::string> const &prioritizedKeys);
 
 #endif
--- src/Tile.cpp
+++ src/Tile.cpp
@@ -6,3 +6,29 @@ Tile::Tile() {
   m_position.z() = 0.0;
   m_size = Vector3::Constant(15);
 }
+
+struct TileOrder {
+  TileOrder (std::vector<std::string> const &prioritizedKeys) : m_prioritizedKeys(prioritizedKeys) { }
+  bool operator () (std::shared_ptr<Tile> const &lhs, std::shared_ptr<Tile> const &rhs) {
+    int compareResult = 0;
+    for (auto it = m_prioritizedKeys.begin(), it_end = m_prioritizedKeys.end(); it != it_end; ++it) {
+      std::string const &key = *it;
+      Value const &lhs_metadata = lhs->m_node->metadata()[key];
+      Value const &rhs_metadata = rhs->m_node->metadata()[key];
+      // the value for key in the hash might be IsNull, which is fine.  not all nodes
+      // should be expected to have completely identical keys.  the compare function
+      // also takes into account the Value type.
+      compareResult = lhs_metadata.compare(rhs_metadata);
+      if (compareResult != 0) {
+        break;
+      }
+    }
+    return compareResult < 0; // if compare returns a negative number, then lhs < rhs.
+  }
+private:
+  std::vector<std::string> const m_prioritizedKeys;
+};
+
+void SortTiles (TilePointerVector &v, std::vector<std::string> const &prioritizedKeys) {
+  std::sort(v.begin(), v.end(), TileOrder(prioritizedKeys));
+}
